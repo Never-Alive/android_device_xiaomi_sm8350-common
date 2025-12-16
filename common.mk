3ac6630635afd5f567637f9be7971c02d3bf98de
Silesh K Nair <sileshn@gmail.com>
2025-12-16 04:36:06 +0100

    sm8350-common: Shim _ZN4Json5ValueC1Ex for libmisight
    This fixes the following compilation error:
    
    vendor/xiaomi/sm8350-common/proprietary/vendor/lib64/libmisight.so: error: Unresolved symbol: _ZN4Json5ValueC1Ex
    
    [Tashar02: Fix copyright header style, reword commit message and description]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- common.mk
+++ common.mk
@@ -414,6 +414,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal
 
+# Shim
+PRODUCT_PACKAGES += \
+    libmisightjson_shim
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
